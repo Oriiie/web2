2a71eb0bc651096796d0b3195c464501234b0210
Oriiie <46246359+Oriiie@users.noreply.github.com>
2020-12-05 02:35:41 +0200

    Cоздана стартовая страница

--- chatbot/admin.py
+++ chatbot/admin.py
@@ -1,10 +1,3 @@
 from django.contrib import admin
 
 # Register your models here.
-
-from .models import *
-
-admin.site.register(List)
-admin.site.register(Homework)
-admin.site.register(Note)
-admin.site.register(ScheduleDay)
--- web2/settings.py
+++ web2/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'mrhy#b4+l*$&wl7fsz03t#sw5x=sh4=if8z6*ut&uqt^_bt__q'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
