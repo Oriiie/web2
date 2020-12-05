f982e56c97fcc35f6d87bc877c9ecb4d2f41e53d
Oriiie <46246359+Oriiie@users.noreply.github.com>
2020-12-05 16:00:38 +0200

    Обновлена стартовая страница, создан список файлов

--- chatbot/urls.py
+++ chatbot/urls.py
@@ -3,4 +3,6 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name="index"),
+    path('update_task/<str:pk>/', views.update_task, name="update_task"),
+    path('delete_task/<str:pk>/', views.delete_task, name="delete_task"),
 ]
