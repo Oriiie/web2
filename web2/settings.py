ea1eea430fc28f65743ca63563c077476d0ccd8b
Oriiie <46246359+Oriiie@users.noreply.github.com>
2020-12-08 18:54:30 +0200

    обновление для деплоя

--- web2/settings.py
+++ web2/settings.py
@@ -13,6 +13,8 @@ import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
+import dj_database_url
+
 BASE_DIR = Path(__file__).resolve().parent.parent
 
 
@@ -88,6 +90,9 @@ DATABASES = {
         'PORT': '5432',
     }
 }
+
+db_from_env = dj_database_url.config()
+DATABASES['default'].update(db_from_env)
 
 
 # Password validation
